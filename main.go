04fec8d7e7b70e7b1954fe3a61eea5c14c1b0950
heartsbot <romainmenke+heartsbot@gmail.com>
2016-06-28 12:54:23 +0200

    hack fix

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 
 	"limbo.services/trace"
 	"limbo.services/trace/dev"
 
 	"golang.org/x/net/context"
 
+	"github.com/gorilla/mux"
 	"github.com/romainmenke/hearts/pkg/fakedb"
 	"github.com/romainmenke/universal-notifier/pkg/wercker"
 	"google.golang.org/grpc"
@@ -26,13 +29,13 @@ var (
 
 func main() {
 
-	go serveRPC()
+	serveR()
 
-	go serveHTTP()
+	serveH()
 
 }
 
-func serveRPC() {
+func serveR() {
 
 	trace.DefaultHandler = dev.NewHandler(nil)
 
@@ -55,7 +58,7 @@ func serveRPC() {
 
 	fmt.Println("Ready to serve clients")
 
-	s.Serve(lis)
+	go s.Serve(lis)
 
 }
 
@@ -215,4 +218,89 @@ func (s *server) newUser(ctx context.Context, message *wercker.WerckerMessage) e
 	}
 
 	return nil
+}
+
+func serveH() {
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/heart/{domain}/{user}/{repo}.json", GetUserJSON)
+	router.HandleFunc("/heart/{domain}/{user}/{repo}.svg", GetUserSVG)
+	router.HandleFunc("/user/{domain}/{user}.json", GetHeartJSON)
+	router.HandleFunc("/user/{domain}/{user}.svg", GetHeartSVG)
+
+	go http.ListenAndServe(":8080", router)
+
+}
+
+func GetUserJSON(w http.ResponseWriter, r *http.Request) {
+
+	ctx := context.Background()
+
+	vars := mux.Vars(r)
+	domain := vars["domain"]
+	name := vars["name"]
+
+	user, err := db.LoadUser(ctx, domain, name)
+	if err != nil {
+		resp := make(map[string]string)
+		resp["message"] = "unknown user"
+		json.NewEncoder(w).Encode(resp)
+	}
+	json.NewEncoder(w).Encode(user)
+}
+
+func GetUserSVG(w http.ResponseWriter, r *http.Request) {
+
+	return
+
+}
+
+func GetHeartJSON(w http.ResponseWriter, r *http.Request) {
+
+	ctx := context.Background()
+
+	vars := mux.Vars(r)
+	domain := vars["domain"]
+	user := vars["user"]
+	repo := vars["repo"]
+
+	heart, err := db.LoadHeart(ctx, domain, user, repo)
+	if err != nil {
+		resp := make(map[string]string)
+		resp["message"] = "unknown repository"
+		json.NewEncoder(w).Encode(resp)
+	}
+	json.NewEncoder(w).Encode(heart)
+}
+
+func GetHeartSVG(w http.ResponseWriter, r *http.Request) {
+
+	ctx := context.Background()
+
+	setResponseHeaderSVG(w)
+
+	vars := mux.Vars(r)
+	domain := vars["domain"]
+	user := vars["user"]
+	repo := vars["repo"]
+
+	heart, err := db.LoadHeart(ctx, domain, user, repo)
+	if err != nil {
+		return
+	}
+
+	svgString := heart.SVG()
+
+	fmt.Fprint(w, svgString)
+
+}
+
+func setResponseHeaderSVG(w http.ResponseWriter) {
+
+	w.Header().Add("Content-Type", "image/svg")
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Access-Control-Expose-Headers", "Content-Type, Cache-Control, Expires, Etag, Last-Modified")
+	w.Header().Add("Cache-Control", "no-cache")
+	w.Header().Add("Pragma", "no-cache")
+
 }
